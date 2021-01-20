4f04d066b8f884cb9b899c9e1a9825288c2985b7
ahmed <ahmedfawzy671998@gmailcom>
2021-01-20 16:12:52 +0200

    Move all root types to index of schema to contain all schema of all types

--- src/schema.ts
+++ src/schema.ts
@@ -1,19 +1,9 @@
-import { makeSchema, objectType, queryType } from 'nexus';
+import { makeSchema } from 'nexus';
 import { join } from 'path';
 import * as allTypes from './schema/index';
 
-const query = queryType({
-  definition(t) {
-    t.string('hello', {
-      resolve(parent, args, ctx) {
-        return "Hello world from server"
-      },
-    })
-  },
-})
-
 export const schema = makeSchema({
-  types: [query, allTypes],
+  types: [allTypes],
   outputs: {
     typegen: join(__dirname, '.', 'nexus-typegen.ts'),
     schema: join(__dirname, '.', 'schema.graphql'),
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -1,2 +1,14 @@
+import { queryType } from 'nexus';
+
+export const query = queryType({
+  definition(t) {
+    t.string('hello', {
+      resolve(parent, args, ctx) {
+        return "Hello world from server"
+      },
+    })
+  },
+})
+
 export * from './user';
 export * from './scalar_types';
