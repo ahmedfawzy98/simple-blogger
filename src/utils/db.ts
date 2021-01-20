d561f7844ba13921dbdec3ce64dd8143d844a648
ahmed <ahmedfawzy671998@gmailcom>
2021-01-20 16:11:24 +0200

    Quick fix with deprecation warnings with mongo

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -4,5 +4,6 @@ export const connect = () => {
   return mongoose.connect('mongodb://localhost:27017/simple-blogging', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   });
 }
